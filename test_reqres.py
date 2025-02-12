2d766554f476170c5fb1fedc9f0f5c8ebe3d81dc
yura.cheglakov <yura.cheglakov@gmail.com>
2025-02-12 23:49:43 +0300

    Тесты на имеющиеся коды ошибок

--- test_reqres.py
+++ test_reqres.py
@@ -23,33 +23,57 @@ print(response.text)
 #         validate(body, schema=json.loads(file.read()))
 
 
-def test_schema_validate_from_variable():
+def test_name_job_200_variable():
     response = requests.get("https://reqres.in/api/users", data={"name": "morpheus", "job": "master"}, params = {"page": 2})
     body = response.json()
     print(body)
 
     assert response.status_code == 200
     validate(body, schema=post_users)
-# #
-#
-def test_job_name_from_request_returns_in_response():
-    job = "Anna"
-    name = "Pin"
 
-    response = requests.post("https://reqres.in/api/users", json={"name": name, "job": job})
-    body = response.json()
-
-    assert body["name"] == name
-    assert body["job"] == job
-#
-#
-def test_get_users_returns_unique_users():
+def test_first_last_name_201():
 
     response = requests.post(
         url="https://reqres.in/api/users",
         json={
                 "first_name": "Anna",
                 "last_name": "Frank"
+            }
+    )
+
+    assert response.status_code == 201
+
+def test_first_last_name_delete_204():
+
+    response = requests.delete(
+        url="https://reqres.in/api/users",
+        json={
+                "first_name": "Anna",
+                "last_name": "Frank"
+            }
+    )
+
+    assert response.status_code == 204
+
+def test_first_last_name_put_404():
+
+    response = requests.put(
+        url="https://reqres.in/api/users",
+        json={
+                "first_name": "Anna",
+                "last_name": "Frank"
+            }
+    )
+
+    assert response.status_code == 404
+
+def test_first_last_name_400():
+
+    response = requests.post(
+        url="https://reqres.in/api/users",
+        json={
+                "first_name": "Anna",
+                "last_name1": "Frank"
             }
     )
 
